0c317fde7e83927336bd5b5cfe0a7793ba820b2f
Tadej Borovšak <tadej.borovsak@xlab.si>
2018-01-02 22:09:48 +0100

    Move attribute access to operation inputs
    According to the official docs, placing the get_attribute function
    call here will evaluate it, but we need to copy the lifecycle
    operation definition to the node template. Ugly and exat opposite of
    DRY.

--- scripts/create_b.py
+++ scripts/create_b.py
@@ -1,7 +1,8 @@
 from cloudify import ctx
+from cloudify.state import ctx_parameters as inputs
 
 ctx.logger.info("Creating node of type B")
 
 props = ctx.node.properties
 ctx.logger.info("property_b1 = {}".format(props["property_b1"]))
-ctx.logger.info("property_b2 = {}".format(props["property_b2"]))
+ctx.logger.info("input_b2 = {}".format(inputs["input_b2"]))
